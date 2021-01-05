0042be4a135b14093e6d7fb6c444450d21e95332
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-01-05 21:20:19 +0300

    Quit if the interface was stopped

--- src/zero-dns.lisp
+++ src/zero-dns.lisp
@@ -18,10 +18,10 @@
   "Start ZeroDNS service.on interface IFACE. If DAEMONIZE is T
 daemonize the process."
   (ensure-socket-accessible)
-  (if (not (get-running-iface iface))
-      (error 'zdns-error
-             :format-control "Interface does not exist or is stopped: ~a"
-             :format-arguments (list iface)))
+  (when (not (get-running-iface iface))
+    (error 'zdns-error
+           :format-control "Interface does not exist or is stopped: ~a"
+           :format-arguments (list iface)))
   (when daemonize
     (daemonize))
   (pzmq:with-context (ctx :max-sockets 32)
@@ -33,19 +33,26 @@ daemonize the process."
                (mapcar #'start-service (list #'start-bookkeeper
                                              #'start-sender
                                              #'start-receiver)))))
-        (flet ((stop-fn (signal info ctx)
-                 (declare (ignore signal info ctx))
-                 (pzmq:send control-socket "quit")
-                 (map nil #'join-thread threads)))
+        (labels ((stop ()
+                   (pzmq:send control-socket "quit")
+                   (map nil #'join-thread threads))
+                 (stop-handler (signal info ctx)
+                   (declare (ignore signal info ctx))
+                   (stop)))
           (when daemonize
-            (sb-sys:enable-interrupt sb-posix:sigterm #'stop-fn)
-            (sb-sys:enable-interrupt sb-posix:sigint  #'stop-fn))
+            (sb-sys:enable-interrupt sb-posix:sigterm #'stop-handler)
+            (sb-sys:enable-interrupt sb-posix:sigint  #'stop-handler))
           ;; SB-SYS:INTERACTIVE-INTERRUPT can be signalled only when
           ;; we are not daemonized. Just call the same function,
-          ;; STOP-FN, to do cleanup.
+          ;; STOP, to do cleanup.
           (handler-case
               (loop
                  while (some #'thread-alive-p threads)
-                 do (sleep 1))
+                 do
+                   (sleep 1)
+                   (when (not (get-running-iface iface))
+                     (format *standard-output*
+                             "Interface ~a is down~%" iface)
+                     (stop)))
             (sb-sys:interactive-interrupt ()
-              (stop-fn nil nil nil))))))))
+              (stop))))))))
